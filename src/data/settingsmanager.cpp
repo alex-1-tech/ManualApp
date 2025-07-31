5a28a60801f18989477b8deca19b7cbbf2966526
alex <chistovreal@gmail.com>
2025-07-31 13:55:27 +0300

    Create new defectoscope model in SettingsManager class.

--- src/data/settingsmanager.cpp
+++ src/data/settingsmanager.cpp
@@ -1,40 +1,90 @@
 #include "settingsmanager.h"
+#include <QDebug>
+#include <QMetaProperty>
 
-SettingsManager::SettingsManager(QObject* parent) 
-    : QObject(parent), m_settings("votum", "ManualApp") 
-{
-}
+SettingsManager::SettingsManager(QObject *parent)
+    : QObject(parent), m_settings("votum", "ManualApp") {}
 
 
-QString SettingsManager::machineSerial() const {
-    return m_settings.value("machineSerial", "").toString();
-}
-
-void SettingsManager::setMachineSerial(const QString& serial) {
-    if (machineSerial() != serial) {
-        m_settings.setValue("machineSerial", serial);
-        emit machineSerialChanged();
+void SettingsManager::debugPrint() const {
+    const QMetaObject *meta = this->metaObject();
+    for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
+        QMetaProperty prop = meta->property(i);
+        QVariant value = prop.read(this);
+        qDebug() << prop.name() << "=" << value;
     }
 }
 
-QString SettingsManager::tabletSerial() const {
-    return m_settings.value("tabletSerial", "").toString();
-}
+void SettingsManager::saveAllSettings() {
+    const QMetaObject *meta = this->metaObject();
+    for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
+        QMetaProperty prop = meta->property(i);
+        if (!prop.isReadable()) continue;
 
-void SettingsManager::setTabletSerial(const QString& serial) {
-    if (tabletSerial() != serial) {
-        m_settings.setValue("tabletSerial", serial);
-        emit tabletSerialChanged();
+        QVariant value = prop.read(this);
+
+        if (value.canConvert<QDate>()) {
+            m_settings.setValue(prop.name(), value.toDate().toString(Qt::ISODate));
+        } else {
+            m_settings.setValue(prop.name(), value);
+        }
     }
 }
 
-QString SettingsManager::evbSerial() const {
-    return m_settings.value("evbSerial", "").toString();
+void SettingsManager::loadAllSettings() {
+    const QMetaObject *meta = this->metaObject();
+    for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
+        QMetaProperty prop = meta->property(i);
+        if (!prop.isWritable()) continue;
+
+        QVariant val = m_settings.value(prop.name());
+
+        if (prop.userType() == QMetaType::QDate) {
+            QDate date = QDate::fromString(val.toString(), Qt::ISODate);
+            prop.write(this, date);
+        } else {
+            prop.write(this, val);
+        }
+    }
 }
 
-void SettingsManager::setEvbSerial(const QString& serial) {
-    if (evbSerial() != serial) {
-        m_settings.setValue("evbSerial", serial);
-        emit evbSerialChanged();
+QJsonObject SettingsManager::toJson() const {
+    QJsonObject obj;
+    const QMetaObject *meta = this->metaObject();
+    for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
+        QMetaProperty prop = meta->property(i);
+        if (!prop.isReadable()) continue;
+
+        QVariant value = prop.read(this);
+
+        // Обработка QDate → строка (ISO 8601)
+        if (value.canConvert<QDate>()) {
+            QDate date = value.toDate();
+            obj[prop.name()] = date.toString(Qt::ISODate);
+        } else {
+            obj[prop.name()] = QJsonValue::fromVariant(value);
+        }
+    }
+    return obj;
+}
+
+void SettingsManager::fromJson(const QJsonObject &obj) {
+    const QMetaObject *meta = this->metaObject();
+    for (int i = meta->propertyOffset(); i < meta->propertyCount(); ++i) {
+        QMetaProperty prop = meta->property(i);
+        if (!prop.isWritable()) continue;
+
+        QString name = prop.name();
+        if (!obj.contains(name)) continue;
+
+        QVariant value;
+
+        if (prop.userType() == QMetaType::QDate) {
+            value = QDate::fromString(obj[name].toString(), Qt::ISODate);
+        } else {
+            value = obj[name].toVariant();
+        }
+
+        prop.write(this, value);
     }
 }
--- src/data/settingsmanager.h
+++ src/data/settingsmanager.h
@@ -1,32 +1,84 @@
 #pragma once
+#include <QDate>
+#include <QJsonObject>
 #include <QObject>
 #include <QSettings>
 
+#define DEFINE_SETTING(Type, Name, Default)                                    \
+  Q_PROPERTY(Type Name READ Name WRITE set##Name NOTIFY Name##Changed)         \
+public:                                                                        \
+  Type Name() const { return m_settings.value(#Name, Default).value<Type>(); } \
+  void set##Name(const Type &value) {                                          \
+    if (value != Name()) {                                                     \
+      m_settings.setValue(#Name, value);                                       \
+      emit Name##Changed();                                                    \
+    }                                                                          \
+  }                                                                            \
+  Q_SIGNAL void Name##Changed();
+
+#define DEFINE_DATE_SETTING(Name)                                              \
+  Q_PROPERTY(QDate Name READ Name WRITE set##Name NOTIFY Name##Changed)        \
+public:                                                                        \
+  QDate Name() const {                                                         \
+    return QDate::fromString(m_settings.value(#Name).toString(), Qt::ISODate); \
+  }                                                                            \
+  void set##Name(const QDate &value) {                                         \
+    if (value != Name()) {                                                     \
+      m_settings.setValue(#Name, value.toString(Qt::ISODate));                 \
+      emit Name##Changed();                                                    \
+    }                                                                          \
+  }                                                                            \
+  Q_SIGNAL void Name##Changed();
+
 class SettingsManager : public QObject {
-    Q_OBJECT
-    Q_PROPERTY(QString machineSerial READ machineSerial WRITE setMachineSerial NOTIFY machineSerialChanged)
-    Q_PROPERTY(QString tabletSerial READ tabletSerial WRITE setTabletSerial NOTIFY tabletSerialChanged)
-    Q_PROPERTY(QString evbSerial READ evbSerial WRITE setEvbSerial NOTIFY evbSerialChanged)
+  Q_OBJECT
+  // Registration data
+  DEFINE_SETTING(QString, serialNumber, QString())
+  DEFINE_DATE_SETTING(shipmentDate)
+  DEFINE_SETTING(QString, caseNumber, QString())
+
+  // Main components
+  DEFINE_SETTING(QString, firstPhasedArrayConverters, QString())
+  DEFINE_SETTING(QString, secondPhasedArrayConverters, QString())
+  DEFINE_SETTING(QString, batteryCase, QString())
+
+  // Blocks and modules
+  DEFINE_SETTING(QString, aosBlock, QString())
+  DEFINE_SETTING(QString, flashDrive, QString())
+  DEFINE_SETTING(QString, coThreeRMeasureChanged, QString())
+
+  // Certification and checks
+  DEFINE_SETTING(QString, calibrationCertificate, QString())
+  DEFINE_DATE_SETTING(calibrationDate)
+
+  // Spare parts kit
+  DEFINE_SETTING(bool, hasTabletScrews, false)
+  DEFINE_SETTING(bool, hasEthernetCable, false)
+  DEFINE_SETTING(QString, batteryCharger, QString())
+  DEFINE_SETTING(QString, tabletCharger, QString())
+  DEFINE_SETTING(bool, hasToolKit, false)
+
+  // Inspection and documentation
+  DEFINE_SETTING(QString, softwareCheck, QString())
+  DEFINE_SETTING(QString, photoVideoUrl, QString())
+  DEFINE_SETTING(double, weight, 0.0)
+  DEFINE_SETTING(QString, notes, QString())
+
+  // Additional components
+  DEFINE_SETTING(QString, manualInclined, QString())
+  DEFINE_SETTING(QString, straight, QString())
+  DEFINE_SETTING(QString, photoUrl, QString())
 
 public:
-    explicit SettingsManager(QObject* parent = nullptr);
-    ~SettingsManager() {
-    }
+  explicit SettingsManager(QObject *parent = nullptr);
+  ~SettingsManager() = default;
 
-    QString machineSerial() const;
-    void setMachineSerial(const QString& serial);
-
-    QString tabletSerial() const;
-    void setTabletSerial(const QString& serial);
-
-    QString evbSerial() const;
-    void setEvbSerial(const QString& serial);
-
-signals:
-    void machineSerialChanged();
-    void tabletSerialChanged();
-    void evbSerialChanged();
+  void debugPrint() const;
+  void saveAllSettings();
+  void loadAllSettings();
+  QJsonObject toJson() const;
+  void fromJson(const QJsonObject &obj);
 
 private:
-    QSettings m_settings;
+  QSettings m_settings;
 };
